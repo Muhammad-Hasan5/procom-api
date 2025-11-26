3eeeec0c4223908e573aef1497e0c641559f1a38
Muhammad Hasan <hasanamir8901@gmail.com>
2025-11-27 04:35:20 +0500

    setup database connection

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./db/index.js";
 dotenv.config({
     path: "./.env",
 });
 let port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+connectDB()
+    .then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+})
+    .catch((error) => {
+    console.error("Something went wrong with Database", error);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./db/index.js";
 
 dotenv.config({
 	path: "./.env",
 });
 
 let port = process.env.PORT;
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
-});
+
+connectDB()
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Server is running on http://localhost:${port}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Something went wrong with Database", error);
+	});
