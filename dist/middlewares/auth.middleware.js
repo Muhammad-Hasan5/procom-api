4135104b77f8ab524dc9572000edcb52deb0fae9
Muhammad Hasan <hasanamir8901@gmail.com>
2025-11-30 05:28:56 +0500

    add auth middleware

---
+++ dist/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import { ApiErrorResponse } from "../utils/api-error-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/users.model.js";
+import jwt from "jsonwebtoken";
+export const authMiddleware = asyncHandler(async (req, res, next) => {
+    const accessToken = req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", "");
+    if (!accessToken) {
+        throw new ApiErrorResponse(404, "Access token not found, logged in user not validated");
+    }
+    try {
+        const decodedAccessToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        if (!decodedAccessToken) {
+            throw new ApiErrorResponse(401, "Unable to decode the fetched token");
+        }
+        const user = await User.findById(decodedAccessToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+        if (!user) {
+            throw new ApiErrorResponse(404, "user not found");
+        }
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        throw new ApiErrorResponse(401, "Unable to decode the fetched token");
+    }
+});
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiErrorResponse } from "../utils/api-error-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/users.model.js";
+import jwt from "jsonwebtoken";
+import { AccessTokenPayload } from "../types/TokensPayload.js";
+
+export const authMiddleware = asyncHandler(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const accessToken =
+			req.cookies?.accessToken ||
+			req.header("Authorization")?.replace("Bearer ", "");
+
+		if (!accessToken) {
+			throw new ApiErrorResponse(
+				404,
+				"Access token not found, logged in user not validated",
+			);
+		}
+
+		try {
+			const decodedAccessToken = jwt.verify(
+				accessToken,
+				process.env.ACCESS_TOKEN_SECRET as string,
+			) as AccessTokenPayload;
+
+			if (!decodedAccessToken) {
+				throw new ApiErrorResponse(
+					401,
+					"Unable to decode the fetched token",
+				);
+			}
+
+			const user = await User.findById(decodedAccessToken?._id).select(
+				"-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+			);
+
+			if (!user) {
+				throw new ApiErrorResponse(404, "user not found");
+			}
+
+			req.user = user;
+			next();
+		} catch (error) {
+			throw new ApiErrorResponse(
+				401,
+				"Unable to decode the fetched token",
+			);
+		}
+	},
+);
