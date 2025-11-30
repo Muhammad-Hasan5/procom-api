32e5757366ab6fd839c8dabdda0900004b5766c2
Muhammad Hasan <hasanamir8901@gmail.com>
2025-11-30 20:59:28 +0500

    add mail sending function

--- dist/utils/mail.js
+++ dist/utils/mail.js
@@ -1,63 +1,18 @@
-import Mailgen from "mailgen";
-import nodemailer from "nodemailer";
-import { MailtrapTransport } from "mailtrap";
-export const sendEmail = async (params) => {
-    const mailGenerator = new Mailgen({
-        theme: "neopolitan",
-        product: {
-            name: "Notion-lite",
-            link: "http://Notion-lite.com",
-        },
-    });
-    const textEmail = mailGenerator.generatePlaintext(params.mailgenContent);
-    const HTML_Email = mailGenerator.generate(params.mailgenContent);
-    const transport = nodemailer.createTransport(MailtrapTransport({
-        token: process.env.MAILTRAP_TOKEN,
-    }));
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
+export const sendEmail = async ({ to, subject, html, }) => {
     try {
-        await transport.sendMail({
-            from: "team@notionlite.com",
-            to: params.email,
-            subject: params.subject,
-            text: textEmail,
-            html: HTML_Email,
+        const response = await resend.emails.send({
+            from: `${process.env.SENDER_NAME} <${process.env.SENDER_EMAIL}>`,
+            to,
+            subject,
+            html,
         });
+        console.log("Email sent:", response);
+        return response;
     }
     catch (error) {
-        console.error("Error sending email, maybe mailtrap api issue", error);
+        console.log("Error email sending:", error);
+        throw error;
     }
-};
-export const emailVerificationMailgenContent = (username, verificationURL) => {
-    return {
-        body: {
-            name: username,
-            intro: "Welcome to Notion-lite, We're happy to have you on board.",
-            action: {
-                instructions: "To get started with Notion-lite, please click here: ",
-                button: {
-                    color: "#22BC66",
-                    text: "Verify your email",
-                    link: verificationURL,
-                },
-            },
-            outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
-        },
-    };
-};
-export const passwordForgotMailgenContent = (username, resetPasswordURL) => {
-    return {
-        body: {
-            name: username,
-            intro: "Want to reset your password?",
-            action: {
-                instructions: "Click below to set new password",
-                button: {
-                    color: "#22BC66",
-                    text: "Reset password",
-                    link: resetPasswordURL,
-                },
-            },
-            outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
-        },
-    };
 };
