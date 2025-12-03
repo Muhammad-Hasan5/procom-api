93576bea635e4142217c9130463075ce09d3696c
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-03 22:05:07 +0500

    add notes validators

---
+++ dist/validators/notes.validator.js
@@ -0,0 +1,7 @@
+import { body } from "express-validator";
+export const notesValidator = () => {
+    return [
+        body("title").trim().notEmpty().withMessage("title is required"),
+        body("content").trim(),
+    ];
+};
