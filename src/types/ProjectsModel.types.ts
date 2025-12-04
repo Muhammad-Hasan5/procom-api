083f1dad93816aff7280ef75c465d725e4f3456b
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-05 02:09:36 +0500

    add project model or schema types

---
+++ src/types/ProjectsModel.types.ts
@@ -0,0 +1,15 @@
+import { Model, Document, Types } from "mongoose";
+
+export interface IProject {
+	title: string;
+	description?: string;
+	owner: Types.ObjectId;
+	members?: Types.ObjectId[];
+}
+
+export interface IProjectDocument extends IProject, Document {
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface IProjectModel extends Model<IProjectDocument> {}
