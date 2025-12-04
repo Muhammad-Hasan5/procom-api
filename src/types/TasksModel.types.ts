dd154991e80169854300071ff58424226de5d3b7
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-05 02:10:09 +0500

    add task model or schema types

---
+++ src/types/TasksModel.types.ts
@@ -0,0 +1,17 @@
+import { Document, Model, Types } from "mongoose";
+
+export interface ITask {
+	title: string;
+	description: string;
+	project: Types.ObjectId;
+	assignedTo: Types.ObjectId;
+	status: "todo" | "in-progress" | "review" | "done";
+	createdBy: Types.ObjectId;
+}
+
+export interface ITaskDocument extends ITask, Document {
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface ITaskModel extends Model<ITaskDocument> {}
