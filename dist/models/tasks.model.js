c6fbcf6c59349d0fb54b75559465eea5a9da8800
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-05 02:09:49 +0500

    create task model

---
+++ dist/models/tasks.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Types } from "mongoose";
+export const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+    },
+    project: {
+        type: Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    assignedTo: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["todo", "in-progress", "review", "done"],
+        default: "todo",
+    },
+    createdBy: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+export const Task = mongoose.model("Task", taskSchema);
---
+++ src/models/tasks.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Types } from "mongoose";
+import { ITaskDocument, ITaskModel } from "../types/TasksModel.types.js";
+
+export const taskSchema = new Schema<ITaskDocument>(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			default: "",
+		},
+		project: {
+			type: Types.ObjectId,
+			ref: "Project",
+			required: true,
+		},
+		assignedTo: {
+			type: Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: ["todo", "in-progress", "review", "done"],
+			default: "todo",
+		},
+		createdBy: {
+			type: Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+export const Task = mongoose.model<ITaskDocument, ITaskModel>(
+	"Task",
+	taskSchema,
+);
