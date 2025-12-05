a688dbd4cdc290d30a9149dbba024f929bcb43b7
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-05 05:18:03 +0500

    add task, note, project to express request interface

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,15 @@
+import { NoteDocument } from "./NotesModel.types.ts";
+import { ProjectDocument } from "./ProjectsModel.types.ts";
+import { TaskDocument } from "./TasksModel.types.ts";
 import { UserDocument } from "./UserModel.types.ts";
 
 declare global {
 	namespace Express {
 		interface Request {
 			user?: UserDocument;
+			project?: ProjectDocument;
+			note?: NoteDocument;
+			task?: TaskDocument;
 		}
 	}
 }
