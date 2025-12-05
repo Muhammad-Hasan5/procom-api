6eaabb09f63713bb4ae43f28358c7198cc7db350
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-05 05:17:00 +0500

    add task middleware

---
+++ dist/middlewares/task.middleware.js
@@ -0,0 +1,18 @@
+import { Task } from "../models/tasks.model.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiErrorResponse } from "../utils/api-error-response.js";
+import { Types } from "mongoose";
+export const canAccessTask = asyncHandler(async (req, res, next) => {
+    if (!Types.ObjectId.isValid(req.params.taskId)) {
+        throw new ApiErrorResponse(404, "invalid project ID");
+    }
+    const task = await Task.findById(req.params.taskId);
+    if (!task) {
+        throw new ApiErrorResponse(404, "Task not found");
+    }
+    if (!task.project?.equals(req.project?._id)) {
+        throw new ApiErrorResponse(403, "task not belong to this project");
+    }
+    req.task = task;
+    next();
+});
