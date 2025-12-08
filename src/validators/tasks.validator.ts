011b756d429f215218d24b50abf74138bf1e277c
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-08 23:10:30 +0500

    add tasks validtors

---
+++ src/validators/tasks.validator.ts
@@ -0,0 +1,31 @@
+import { body } from "express-validator";
+
+export const taskValidator = () => {
+	return [
+		body("title")
+			.notEmpty()
+			.withMessage("Task title is required")
+			.isString()
+			.withMessage("Title must be a string")
+			.trim(),
+
+		body("description")
+			.optional()
+			.isString()
+			.withMessage("Description must be a string"),
+
+		body("assignedTo")
+			.notEmpty()
+			.withMessage("assignedTo is required")
+			.isEmail()
+			.withMessage("assignedTo must be a valid email")
+			.bail()
+			.toLowerCase()
+			.trim(),
+
+		body("status")
+			.optional()
+			.isIn(["todo", "in-progress", "review", "done"])
+			.withMessage("Invalid task status"),
+	];
+};
