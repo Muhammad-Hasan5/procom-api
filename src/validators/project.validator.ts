5231fb14cf08428d66769d7c507c5968fa6a424d
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-08 23:10:16 +0500

    add project validators

---
+++ src/validators/project.validator.ts
@@ -0,0 +1,29 @@
+import { body } from "express-validator";
+
+export const projectValidator = () => {
+	return [
+		body("title")
+			.notEmpty()
+			.withMessage("Project title is required")
+			.isString()
+			.withMessage("Title must be a string")
+			.trim(),
+
+		body("description")
+			.optional()
+			.isString()
+			.withMessage("Description must be a string"),
+
+		body("members")
+			.optional()
+			.isArray()
+			.withMessage("Members must be an array"),
+
+		body("members.*")
+			.optional()
+			.isEmail()
+			.withMessage("Each member must be a valid email")
+			.bail()
+			.customSanitizer((email) => email.toLowerCase().trim()),
+	];
+};
