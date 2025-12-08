061c91e17f74fdec22a05de990b19f36d1748a8d
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-08 23:09:31 +0500

    add project routes

---
+++ dist/routes/project.route.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { createProject, updateProject, deleteProject, getSingleProject, getUserProjects, addMemberToProject, removeMemberFromProject, } from "../controllers/project.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { projectValidator } from "../validators/project.validator.js";
+import { validate } from "../middlewares/validators.middleware.js";
+import { canAccessProject } from "../middlewares/project.middleware.js";
+const router = Router();
+// Create & get all projects of logged-in user
+router
+    .route("/projects")
+    .post(authMiddleware, projectValidator(), validate, createProject)
+    .get(authMiddleware, getUserProjects);
+// Get, update, delete one project
+router
+    .route("/projects/:projectId")
+    .get(authMiddleware, canAccessProject, getSingleProject)
+    .patch(authMiddleware, canAccessProject, projectValidator(), validate, updateProject)
+    .delete(authMiddleware, canAccessProject, deleteProject);
+// Manage project members
+router
+    .route("/projects/:projectId/members")
+    .post(authMiddleware, canAccessProject, addMemberToProject);
+router
+    .route("/projects/:projectId/members/:memberId")
+    .delete(authMiddleware, canAccessProject, removeMemberFromProject);
+export default router;
